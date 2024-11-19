f80b4f8f5cc9d52908798de852c3fc0e4b09bdce
bink0sss <127861415+bink0sss@users.noreply.github.com>
2024-11-19 09:26:02 +0100

    Update rezerwacje.py

--- c2/rezerwacje.py
+++ c2/rezerwacje.py
@@ -1,7 +1,7 @@
-# ### 1. Wyświetlanie aktualnego stanu rezerwacji miejsc
-# - **Typ danych:** lista (`seats`)
-# - **Opis:** Używamy listy `seats`, która przechowuje informacje o rezerwacjach miejsc. Jeśli miejsce jest wolne, w liście znajduje się wartość `None`. Jeśli miejsce jest zarezerwowane, w liście przechowywane jest imię osoby, która je zarezerwowała.
-# - **Funkcja:** `print_seats(seats)` wyświetla aktualny stan wszystkich miejsc, informując, które z nich są wolne, a które są zarezerwowane oraz przez kogo.
+  1. Wyświetlanie aktualnego stanu rezerwacji miejsc
+ - **Typ danych:** lista (`seats`)
+ - **Opis:** Używamy listy `seats`, która przechowuje informacje o rezerwacjach miejsc. Jeśli miejsce jest wolne, w liście znajduje się wartość `None`. Jeśli miejsce jest zarezerwowane, w liście przechowywane jest imię osoby, która je zarezerwowała.
+ - **Funkcja:** `print_seats(seats)` wyświetla aktualny stan wszystkich miejsc, informując, które z nich są wolne, a które są zarezerwowane oraz przez kogo.
 
 def print_seats(seats):
     for index, seat in enumerate(seats):
